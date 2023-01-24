8d9c888e8178d7514b02172ccc89ccb4bb5e02ad
itsmiraz <mirajulislamofficial044@gmail.com>
2023-01-24 10:51:13 +0600

    bux fixed

--- Index.js
+++ Index.js
@@ -11,10 +11,6 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
